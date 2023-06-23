71ebc52f028ef4e5f4418963556a764c7e85d4a1
Niraj Pandey <nirajpandey034@gmail.com>
2023-06-23 15:46:23 +0530

    added content post route

--- routes/contentRoute.js
+++ routes/contentRoute.js
@@ -5,20 +5,30 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const ContentModel = require("../models/Content");
 
-router.get("/get_content", async (req, response, next) => {
+router.get("/get_content", async (req, res, next) => {
   try {
     await ContentModel.find({})
       .then((contents) => {
-        response.json({ data: contents });
+        res.json({ data: contents });
       })
       .catch((error) => {
-        response.json({ error: error });
+        res.json({ error: error });
       });
   } catch (error) {
-    response.status(500).send("Error Occured: " + error.message);
+    res.status(500).send("Error Occured: " + error.message);
   }
 });
 
+router.post("/post_content", async (req, res) => {
+  const content = new ContentModel(req.body);
+  console.log(content);
+  try {
+    await content.save();
+    res.status(200).json({ info: `${content.title} is added successfully` });
+  } catch (error) {
+    res.status(500).json({ error: "Error Occured: " + error.message });
+  }
+});
 router.get("/", (req, res, next) => {
   res.send("Welcome Sir");
 });
